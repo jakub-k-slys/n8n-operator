e65a1f65ebf1a6bbf612bdff450e724f177de15d
Jakub Slys <jakub@slys.dev>
2025-05-28 00:03:20 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'N8n Kubernetes Operator'
+copyright = '2025, slys.dev'
+author = 'Jakub Slys'
 
 release = '0.1'
 version = '0.1.0'
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
